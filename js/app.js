25e69f7466a5d61a6e3f82907e7d6e17533588fe
crystdang <111620460+crystdang@users.noreply.github.com>
2022-11-16 16:11:01 -0500

    Module 12.5.2 complete

---
+++ js/app.js
@@ -0,0 +1,77 @@
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+// Simple JavaScript console.log statement
+function printHello() {
+    console.log("Hello there!");
+  }
+
+// Takes two numbers and adds them
+function addition(a, b) {
+    return a + b;
+  }
+
+
+console.log(addition(4, 5));
+
+// Functions can call other functions
+function doubleAddition(c, d) {
+    var total = addition(c, d) * 2;
+    return total;
+  }
+
+console.log(doubleAddition(65, 34));
+
+// clear() to clear console, but previous available by up and down arrow
+
+// fat arrow removes function, curly brackets and return
+printHello = () => "Hello there!"; 
+
+// Converted to an arrow function
+addition = (a, b) => a + b;
+
+doubleAddition = (c, d) => addition(c, d) * 2;
+
+console.log(doubleAddition(33, 25));
+
+let friends = ["Sarah", "Greg", "Cindy", "Jeff"];
+
+function listLoop(userList) {
+    for (var i = 0; i < userList.length; i++) {
+      console.log(userList[i]);
+    }
+ }
+
+ let vegetables = ["Carrots", "Peas", "Lettuce", "Tomatoes"];
+
+ for (var i = 0; i < vegetables.length; i++) {
+    console.log("I love " + vegetables[i]);
+}
+
+for (var i = 0; i < 5; i++) {
+    console.log("I am " + i);
+ }
+
+ function buildTable(data) {
+    // First, clear out any existing data
+    tbody.html("");
+  
+    // Next, loop through each object in the data
+    // and append a row and cells for each value in the row
+    data.forEach((dataRow) => {
+      // Append a row to the table body
+      let row = tbody.append("tr");
+  
+      // Loop through each field in the dataRow and add
+      // each value as a table cell (td)
+      Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td");
+        cell.text(val);
+        }
+      );
+    });
+  }
+  
