d0345ecc394055f78dfdfdcf6a36c853cd82c969
crystdang <111620460+crystdang@users.noreply.github.com>
2022-11-16 20:02:06 -0500

    Deliverable 1 incomplete

---
+++ static/js/app_1.js
@@ -0,0 +1,104 @@
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+// Simple JavaScript console.log statement
+function printHello() {
+    console.log("Hello there!");
+  }
+
+// Takes two numbers and adds them
+function addition(a, b) {
+    return a + b;
+  }
+
+
+console.log(addition(4, 5));
+
+// Functions can call other functions
+function doubleAddition(c, d) {
+    var total = addition(c, d) * 2;
+    return total;
+  }
+
+console.log(doubleAddition(65, 34));
+
+// clear() to clear console, but previous available by up and down arrow
+
+// fat arrow removes function, curly brackets and return
+printHello = () => "Hello there!"; 
+
+// Converted to an arrow function
+addition = (a, b) => a + b;
+
+doubleAddition = (c, d) => addition(c, d) * 2;
+
+console.log(doubleAddition(33, 25));
+
+let friends = ["Sarah", "Greg", "Cindy", "Jeff"];
+
+function listLoop(userList) {
+    for (var i = 0; i < userList.length; i++) {
+      console.log(userList[i]);
+    }
+ }
+
+ let vegetables = ["Carrots", "Peas", "Lettuce", "Tomatoes"];
+
+ for (var i = 0; i < vegetables.length; i++) {
+    console.log("I love " + vegetables[i]);
+}
+
+for (var i = 0; i < 5; i++) {
+    console.log("I am " + i);
+ }
+
+ function buildTable(data) {
+    // First, clear out any existing data
+    tbody.html("");
+  
+    // Next, loop through each object in the data
+    // and append a row and cells for each value in the row
+    data.forEach((dataRow) => {
+      // Append a row to the table body
+      let row = tbody.append("tr");
+  
+      // Loop through each field in the dataRow and add
+      // each value as a table cell (td)
+      Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td");
+        cell.text(val);
+        }
+      );
+    });
+  }
+  
+// if-statement syntax
+// if ( condition ) { code to execute }
+
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+  
+     // Check to see if a date was entered and filter the
+    // data using that date.
+    if (date) {
+      // Apply `filter` to the table data to only keep the
+      // rows where the `datetime` value matches the filter value
+      filteredData = filteredData.filter(row => row.datetime === date);
+    };
+  
+     // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+  };
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
